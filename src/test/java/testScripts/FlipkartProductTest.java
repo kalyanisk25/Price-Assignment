cffc4532e2ea9165013ec6f8ba0427d60d5ee248
Chaya P S <chaya.ps@appify.com>
2022-03-19 12:42:42 +0530

    initial commit

---
+++ src/test/java/testScripts/FlipkartProductTest.java
@@ -0,0 +1,41 @@
+package testScripts;
+
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Library.SmallCommon;
+import baseTest.TestBase;
+import businessHelpers.AmazonFunction;
+import businessHelpers.FlipkartFunction;
+
+public class FlipkartProductTest extends TestBase{
+	FlipkartFunction objFlipkartFunction;
+	AmazonFunction objAmazonFunction;
+	SmallCommon objSmallCommon;
+	
+	@BeforeSuite(alwaysRun = true)
+	public void beforeSuite()
+	{
+		objFlipkartFunction = new FlipkartFunction();
+		objAmazonFunction = new AmazonFunction();
+		objSmallCommon = new SmallCommon();
+	}
+	
+	//@Test(priority = 0 , description = "Validate Product Search and add to cart feature on flipkart") 
+	@Test
+	public void firstTestCase()
+	{
+		objFlipkartFunction.navigateToFlipkart();
+		objFlipkartFunction.searchProduct("Apple iPhone XR (Yellow, 64 GB)");
+		String prodName = objFlipkartFunction.verifyProductSelection();
+		System.out.println("Product Name" + prodName);
+		Float prodPrice = objFlipkartFunction.fetchProductPrice();
+		System.out.println("Product Price" + prodPrice);
+		objFlipkartFunction.addProductToCart();
+	}
+	
+	
+
+}
---
+++ src/test/java/testScripts/PriceComparisionTest.java
@@ -0,0 +1,48 @@
+package testScripts;
+
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import Library.SmallCommon;
+import baseTest.TestBase;
+import businessHelpers.AmazonFunction;
+import businessHelpers.FlipkartFunction;
+
+public class PriceComparisionTest extends TestBase{
+	FlipkartFunction objFlipkartFunction;
+	AmazonFunction objAmazonFunction;
+	SmallCommon objSmallCommon;
+	
+	@BeforeSuite(alwaysRun = true)
+	public void beforeSuite()
+	{
+		objFlipkartFunction = new FlipkartFunction();
+		objAmazonFunction = new AmazonFunction();
+		objSmallCommon = new SmallCommon();
+	}
+	
+
+	//@Test (priority = 0 , description = "Validate Product Search and compare prices on flipkart & Amazon")
+	@Test
+	public void secondTestCase()
+	{
+		objFlipkartFunction.navigateToFlipkart();
+		objFlipkartFunction.searchProduct("Apple iPhone XR (Yellow, 64 GB)");
+		String flipKartProductName = objFlipkartFunction.verifyProductSelection();
+		Float flipkartProdPrice = objFlipkartFunction.fetchProductPrice();
+		System.out.println("Flipkart product price " + flipkartProdPrice);
+		
+		objAmazonFunction.navigateToAmazon();
+		objAmazonFunction.searchProduct("Apple iPhone XR (Yellow, 64 GB)");
+		objAmazonFunction.selectProd(flipKartProductName);
+		Float amazonProdPrice = objAmazonFunction.FetchPrice();	
+		System.out.println("Amazon Product Price " + amazonProdPrice);
+		
+		objSmallCommon.comparePrices(flipkartProdPrice,amazonProdPrice);
+	}
+
+	
+
+}
